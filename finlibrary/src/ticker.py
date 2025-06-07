1e472ded7f87886dd2ff6283975e064f4ec061c1
prangel-git <62568827+prangel-git@users.noreply.github.com>
2025-06-07 13:54:21 -0400

    ticker updates its dependecies

--- finlibrary/src/ticker.py
+++ finlibrary/src/ticker.py
@@ -2,6 +2,29 @@
 class Ticker:
     def __init__(self):
         self.period = 0
+        self.dependencies = []
 
     def tick(self):
         self.period += 1
+        for dependencies in self.dependencies:
+            dependencies.update()
+
+    def register(self, ticker_dependent):
+        self.dependencies.append(ticker_dependent)
+
+
+class TickerDependent:
+    def __init__(self, ticker):
+        ticker.register(self)
+    
+    def update():
+        pass
+
+
+class MockTickerDependent(TickerDependent):
+    def __init__(self, ticker):
+        super().__init__(ticker)
+        self.times_updated = 0
+    
+    def update(self):
+        self.times_updated += 1
--- finlibrary/tests/test_ticker.py
+++ finlibrary/tests/test_ticker.py
@@ -1,4 +1,4 @@
-from finlibrary.src.ticker import Ticker
+from finlibrary.src.ticker import Ticker, MockTickerDependent
 
 def test_get_period():
     ticker = Ticker()
@@ -8,3 +8,17 @@ def test_tick_advances_time():
     ticker = Ticker()
     ticker.tick()
     assert ticker.period == 1
+
+def test_ticker_dependent_object_registers_as_a_dependency_of_ticker():
+    ticker = Ticker()
+    ticker_dependent = MockTickerDependent(ticker)
+    assert ticker_dependent == ticker.dependencies[0]
+
+
+
+
+def test_tick_ticks_ticker_dependent_objects():
+    ticker = Ticker()
+    ticker_dependent = MockTickerDependent(ticker)
+    ticker.tick()
+    assert ticker_dependent.times_updated == 1
