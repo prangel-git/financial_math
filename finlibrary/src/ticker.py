49dba1c5e1045df6cd50023f93a3539952f993cd
prangel-git <62568827+prangel-git@users.noreply.github.com>
2025-06-07 13:29:24 -0400

    ticker period advances

--- finlibrary/src/ticker.py
+++ finlibrary/src/ticker.py
@@ -2,3 +2,6 @@
 class Ticker:
     def __init__(self):
         self.period = 0
+
+    def tick(self):
+        self.period += 1
--- finlibrary/tests/test_ticker.py
+++ finlibrary/tests/test_ticker.py
@@ -3,3 +3,8 @@ from finlibrary.src.ticker import Ticker
 def test_get_period():
     ticker = Ticker()
     assert ticker.period == 0
+
+def test_tick_advances_time():
+    ticker = Ticker()
+    ticker.tick()
+    assert ticker.period == 1
