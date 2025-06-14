0e2f3d669c502fda9061f76c9e29f6c8a97ddea7
prangel-git <62568827+prangel-git@users.noreply.github.com>
2025-06-14 14:28:30 -0400

    multiperiod binomial tree

--- finlibrary/src/binomial_tree.py
+++ finlibrary/src/binomial_tree.py
@@ -1,3 +1,6 @@
+def binomial_tree_values(initial_value, up_factor, down_factor, period):
+    return [(up_factor ** k) * (down_factor ** (period - k)) for k in range(period+1)]
+
 def one_period_binomial_pricing(
         stock_initial_price,
         up_factor,
@@ -22,3 +25,85 @@ def one_period_binomial_pricing(
     bond_allocation = price - stock_allocation * stock_initial_price
 
     return price, stock_allocation, bond_allocation
+
+
+def multiperiod_binomial_pricing(
+        stock_initial_price,
+        up_factor,
+        down_factor,
+        bond_return,
+        derivative_payoff,
+        derivative_expiry
+    ):
+
+    if derivative_expiry == 0:
+        price = derivative_payoff(stock_initial_price)
+        stock_allocation = 0
+        bond_allocation = 0
+        hedge_tree_up = None
+        hedge_tree_down = None
+    
+        return BinaryTree(
+            price,
+            stock_allocation,
+            bond_allocation,
+            hedge_tree_up,
+            hedge_tree_down
+        )
+    
+    hedge_tree_up = multiperiod_binomial_pricing(
+        stock_initial_price * up_factor,
+        up_factor,
+        down_factor,
+        bond_return,
+        derivative_payoff,
+        derivative_expiry - 1
+    )
+
+    hedge_tree_down = multiperiod_binomial_pricing(
+        stock_initial_price * down_factor,
+        up_factor,
+        down_factor,
+        bond_return,
+        derivative_payoff,
+        derivative_expiry - 1
+    )
+
+    payoff_next_step_dict = {
+        stock_initial_price * up_factor : hedge_tree_up.price,
+        stock_initial_price * down_factor : hedge_tree_down.price
+    }
+
+    payoff_next_step = lambda s_1 : payoff_next_step_dict[s_1]
+
+    price, stock_allocation, bond_allocation = one_period_binomial_pricing(
+        stock_initial_price,
+        up_factor,
+        down_factor,
+        bond_return,
+        payoff_next_step
+    )
+
+    return BinaryTree(
+        price,
+        stock_allocation,
+        bond_allocation,
+        hedge_tree_up,
+        hedge_tree_down
+    ) 
+
+
+
+
+class BinaryTree():
+    def __init__(self, 
+                 price, 
+                 stock_allocation, 
+                 bond_allocation,
+                 up_tree,
+                 down_tree):
+        self.price = price
+        self.stock_allocation = stock_allocation
+        self.bond_allocation = bond_allocation
+        self.up_tree = up_tree
+        self.down_tree = down_tree
--- finlibrary/tests/test_binomial_tree.py
+++ finlibrary/tests/test_binomial_tree.py
@@ -1,5 +1,7 @@
 
-from finlibrary.src.binomial_tree import one_period_binomial_pricing
+import pytest
+
+from finlibrary.src.binomial_tree import one_period_binomial_pricing, multiperiod_binomial_pricing, binomial_tree_values
 
 def test_one_period_binomial_pricing():
     stock_initial_price = 4
@@ -21,3 +23,33 @@ def test_one_period_binomial_pricing():
     assert stock_allocation == 1/2
     assert bond_allocation == -0.8
 
+def test_multiperiod_binomial_pricing():
+    stock_initial_price = 4
+    up_factor = 2
+    down_factor = 1/2
+    bond_return = 5/4
+    derivative_expiry = 3
+    strike = 6
+    derivative_payoff = lambda s_N: max(s_N-strike, 0)
+
+    hedging_tree = multiperiod_binomial_pricing(
+        stock_initial_price,
+        up_factor,
+        down_factor,
+        bond_return,
+        derivative_payoff,
+        derivative_expiry
+    )
+
+    assert hedging_tree.price == pytest.approx(2.048)
+    assert hedging_tree.stock_allocation == pytest.approx(0.7466666666666666)
+    assert hedging_tree.bond_allocation == pytest.approx(-0.9386666666666663) 
+
+
+def test_binomial_tree_values():
+    initial_value = 1
+    up_factor = 2
+    down_factor = 0.5
+    possible_values = binomial_tree_values(initial_value, up_factor, down_factor, 2)
+    expected_values = [0.25, 1, 4]
+    assert possible_values == expected_values
